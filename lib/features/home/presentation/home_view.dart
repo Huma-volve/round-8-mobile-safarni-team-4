f9196d8f2f84a1af8126ca193b8dbf1b1412b058
7oda <7oda@gmail.com>
2026-01-04 16:46:12 +0200

    - add app themeing and routing .

---
+++ lib/features/home/presentation/home_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 24,
+            children: [
+              FilledButton(onPressed: () {}, child: Text('Add')),
+              OutlinedButton(onPressed: () {}, child: Text('Add')),
+
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'full name',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
