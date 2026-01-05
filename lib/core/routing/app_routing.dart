61c09497e2167c7d6a638ac62ea9e3d6046e196f
7oda <7oda@gmail.com>
2026-01-05 23:28:16 +0200

    feat(flight_appointment): add select seat screen .

--- lib/core/routing/app_routing.dart
+++ lib/core/routing/app_routing.dart
@@ -18,10 +18,10 @@ abstract class AppRouter {
       switch (name) {
         case AppRoutes.home:
           return MaterialPageRoute(builder: (context) => HomeView());
-        // case AppRoutes.onboardingView:
-        //   return MaterialPageRoute(
-        //     builder: (context) => const OnboardingView(),
-        //   );
+        case AppRoutes.onboardingView:
+          return MaterialPageRoute(
+            builder: (context) => const OnboardingView(),
+          );
 
         case AppRoutes.paymentMethod:
           return MaterialPageRoute(builder: (context) => const CheckoutView());
