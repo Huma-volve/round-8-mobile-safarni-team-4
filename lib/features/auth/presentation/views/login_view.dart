dca2aa2899fe96fe7add0141eeaa5e7f233caae8
Ebrahim-Elkbbany <136932497+Ebrahim-Elkbbany@users.noreply.github.com>
2026-01-05 14:23:03 +0200

    login screen ui refactor

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:round_8_mobile_safarni_team4/core/helpers/size_config_extension.dart';
 import 'package:round_8_mobile_safarni_team4/features/auth/presentation/views/widgets/don_not_have_account_sec.dart';
 import 'package:round_8_mobile_safarni_team4/features/auth/presentation/views/widgets/login_text_form_fields_sec.dart';
-import 'package:round_8_mobile_safarni_team4/features/auth/presentation/views/widgets/login_image_section.dart';
-import 'package:round_8_mobile_safarni_team4/features/auth/presentation/views/widgets/login_welcome_title_section.dart';
-import 'package:round_8_mobile_safarni_team4/features/auth/presentation/views/widgets/login_forgot_password_section.dart';
-import 'package:round_8_mobile_safarni_team4/features/auth/presentation/views/widgets/login_button_section.dart';
+import 'package:round_8_mobile_safarni_team4/features/auth/presentation/views/widgets/login_image_and_welcome_title_section.dart';
+import 'package:round_8_mobile_safarni_team4/features/auth/presentation/views/widgets/login_forgot_password_and_button_section.dart';
 import 'package:round_8_mobile_safarni_team4/features/auth/presentation/views/widgets/login_divider_section.dart';
 import 'package:round_8_mobile_safarni_team4/features/auth/presentation/views/widgets/login_social_section.dart';
 
@@ -22,15 +20,11 @@ class LoginView extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               context.gapH(40),
-              const LoginImageSection(),
-              context.gapH(36),
-              const LoginWelcomeTitleSection(),
+              const LoginImageAndWelcomeTitleSection(),
               context.gapH(8),
               const LoginTextFormFieldsSec(),
               context.gapH(8),
-              const LoginForgotPasswordSection(),
-              context.gapH(24),
-              const LoginButtonSection(),
+              const LoginForgotPasswordAndButtonSection(),
               context.gapH(24),
               const LoginDividerSection(),
               context.gapH(24),
