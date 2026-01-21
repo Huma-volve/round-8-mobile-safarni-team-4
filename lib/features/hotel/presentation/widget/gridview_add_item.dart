ff50bafdf0a9220655d63c9ba31de62dc416dbf6
mohamed_hussein <mohamedhusseinmans@gmail.com>
2026-01-21 22:37:12 +0200

    add decoration in image selected

--- lib/features/hotel/presentation/widget/gridview_add_item.dart
+++ lib/features/hotel/presentation/widget/gridview_add_item.dart
@@ -20,38 +20,42 @@ class GridviewAddItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      behavior: HitTestBehavior.opaque, // 🔥 IMPORTANT
+      behavior: HitTestBehavior.opaque,
       onTap: onTap,
       onLongPress: onLongPress,
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.file(
-              images,
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: double.infinity,
-            ),
-          ),
-
-          if (showDelete)
-            Positioned(
-              top: 6,
-              right: 6,
-              child: GestureDetector(
-                onTap: () {
-                  // prevent clearing selection when clicking close
-                  onRemove();
-                },
-                child: const CircleAvatar(
-                  radius: 14,
-                  backgroundColor: Colors.red,
-                  child: Icon(Icons.close, color: Colors.white, size: 16),
-                ),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border: showDelete ? Border.all(color: Colors.blue, width: 2) : null,
+        ),
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.file(
+                images,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
               ),
             ),
-        ],
+
+            if (showDelete)
+              Positioned(
+                top: 6,
+                right: 6,
+                child: GestureDetector(
+                  onTap: onRemove,
+                  child: const CircleAvatar(
+                    radius: 14,
+                    backgroundColor: Colors.red,
+                    child: Icon(Icons.close, color: Colors.white, size: 16),
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
